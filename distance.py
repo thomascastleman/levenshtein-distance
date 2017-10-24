f9935afd13792514c95b7f4cf253cf4452c55588
Thomas Castleman <thomas7rex@gmail.com>
2017-10-24 11:44:14 -0400

    Cleaned syntax

--- distance.py
+++ distance.py
@@ -55,125 +55,105 @@ def getListOfIndexMappings(str1, str2, start1, start2):
 
 # return buffered array which works for both str1 and str2
 def getBufferedArrayFormats(str1, str2, indexMapping):
-
+	# get list of tuples: indices of shared chars between strings
 	sharedIndices = sorted([ (key, indexMapping[key]) for key in indexMapping ])
 
+	# init buffer as array of shared chars
 	bufferedTemplate = []
 	for i in sharedIndices:
 		bufferedTemplate.append(str1[i[0]])
 
-	# DEBUG
-	print bufferedTemplate
-	print sharedIndices
-
+	# copy that array into separate templates for each string
 	template1 = bufferedTemplate[:]
 	template2 = bufferedTemplate[:]
 
-	firstIter = True
+	indexInTemplate = 0		# index used to acces chars in each template
 
-	indexInTemplate = 0
-	print "shared incides: ", sharedIndices
+	# for each shared character
 	for i in range(0, len(sharedIndices)):
 
+		curShared = sharedIndices[i]								# current shared character
+		prevShared = sharedIndices[i - 1] if i > 0 else (0, 0)		# previous shared character
 
-		curShared = sharedIndices[i]
-		prevShared = sharedIndices[i - 1] if i > 0 else (0, 0)#  if curShared != (0, 0) else (-1, -1)
-
-
+		# set bufferspace to the greater of the two spaces (from the two strings) between current shared char and previous
 		if curShared[0] - prevShared[0] > curShared[1] - prevShared[1]:
 			bufferspace = curShared[0] - prevShared[0]
 		else:
 			bufferspace = curShared[1] - prevShared[1]
 
+		# exception handling for first shared char
 		if i != 0:
 			bufferspace -= 1
 
-		print "\n\nlooking at shared char '", str1[curShared[0]], "'"
-
-		print "prevshared: ", prevShared
-		print "curShared: ", curShared
-
-		charsInBetween1 = str1[prevShared[0] + 1 if not firstIter else 0 : curShared[0]]
-		charsInBetween2 = str2[prevShared[1] + 1 if not firstIter else 0 : curShared[1]]
-
-		#DEBUG
-		print "In between:"
-		print "1: ", charsInBetween1
-		print "2: ", charsInBetween2
-
-		print "bufferspace: ", bufferspace
-
+		# get all characters betwen current and previous for both strings
+		charsInBetween1 = str1[prevShared[0] + 1 if i != 0 else 0 : curShared[0]]
+		charsInBetween2 = str2[prevShared[1] + 1 if i != 0 else 0 : curShared[1]]
 
 		for j in range(0, bufferspace):
 
+			# insert intermediate chars if they exist or buffer space
+			# for string 1
 			if j < len(charsInBetween1):
 				template1.insert(indexInTemplate, charsInBetween1[len(charsInBetween1)- 1 - j])
 			else:
 				template1.insert(indexInTemplate, None)
 
+			# for string 2
 			if j < len(charsInBetween2):
 				template2.insert(indexInTemplate, charsInBetween2[len(charsInBetween2) - 1 - j])
 			else:
-
 				template2.insert(indexInTemplate, None)
 
+			# for generic buffer template
 			bufferedTemplate.insert(indexInTemplate, None)
-			print "Inserting none at ", indexInTemplate
 
-		print bufferedTemplate
+		indexInTemplate += bufferspace + 1	# update index back up to the next shared char now that buffer space has been added
 
-
-		indexInTemplate += bufferspace + 1
-		print "index updating to ", indexInTemplate
-		firstIter = False
-
-	# now add end buffer if needed
+	# now add buffer space / chars to end if needed
 	end = (len(str1), len(str2))
 	secondToEnd = sharedIndices[len(sharedIndices) - 1]
 
+	# calc bufferspace as greater of the needed space from both strings
 	if end[0] - secondToEnd[0] > end[1] - secondToEnd[1]:
 		bufferspace = end[0] - secondToEnd[0] - 1
 	else:
 		bufferspace = end[1] - secondToEnd[1] - 1
 
-
-	print "end: ", end
-	print "second to end ", secondToEnd
-
+	# get all chars from (last shared) to (end of string)
 	charsBeforeEnd1 = str1[secondToEnd[0] + 1:end[0]]
 	charsBeforeEnd2 = str2[secondToEnd[1] + 1:end[1]]	
 
-	# DEBUG
-	print "before end1: ", charsBeforeEnd1
-	print "before end2: ", charsBeforeEnd2
-
+	# add buffer space
 	for j in range(0, bufferspace):
-			bufferedTemplate.append(None)
 
+			# for string 1
 			if j < len(charsBeforeEnd1):
 				template1.append(charsBeforeEnd1[j])
 			else:
 				template1.append(None)
 
+			# for string 2
 			if j < len(charsBeforeEnd2):
 				template2.append(charsBeforeEnd2[j])
 			else:
 				template2.append(None)
 
-	print "Buffered Template: "
+			bufferedTemplate.append(None) # for generic buffer template
+
+	print "\n\nGeneric buffer: ",
 	print bufferedTemplate
 
-	print "temp1: ", template1
-	print "temp2: ", template2
+	print "Str1 buffer: ", template1
+	print "Str2 buffer: ", template2
 
 	return ( template1, template2 )
 
 def main():
-	w1 = "cat"
-	w2 = "catch"
+	w1 = "lane"
+	w2 = "ale"
 
 	lev = levDistance(w1, w2)
-	print lev
+	print "\nDistance: ", lev
 
 
 
