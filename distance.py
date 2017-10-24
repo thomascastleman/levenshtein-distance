68df60087bbe858c8c0be3ee57012c9ab4a00458
Thomas Castleman <thomas7rex@gmail.com>
2017-10-23 23:41:28 -0400

    Now can get buffered array based on two strings

--- distance.py
+++ distance.py
@@ -76,13 +76,81 @@ def getListOfIndexMappings(str1, str2, start1, start2):
 	print "exiting subproblem"
 	return setOfDicts
 
+# return the char[] form of str1 and str2 which have been buffered as necessary with null positions to allow direct comparison
+def getBufferedArrayFormats(str1, str2, indexMapping):
+
+	sharedIndices = sorted([ (key, indexMapping[key]) for key in indexMapping ])
+
+	bufferedTemplate = []
+	for i in sharedIndices:
+		bufferedTemplate.append(str1[i[0]])
+
+	# DEBUG
+	print bufferedTemplate
+	print sharedIndices
+
+	for i in range(0, len(sharedIndices)):
+
+		curShared = sharedIndices[i]
+		prevShared = sharedIndices[i - 1] if i > 0 else (0, 0) if curShared != (0, 0) else (-1, -1)
+
+		if curShared[0] - prevShared[0] > curShared[1] - prevShared[1]:
+			bufferspace = curShared[0] - prevShared[0] - 1
+		else:
+			bufferspace = curShared[1] - prevShared[1] - 1
+
+		print "bufferspace: ", bufferspace
+
+		for j in range(0, bufferspace):
+			bufferedTemplate.insert(i, None)
+
+		i = i + bufferspace
+
+	# now add front buffer if needed
+	if sharedIndices[0][0] > sharedIndices[0][1]:
+		bufferspace = sharedIndices[0][0]
+	else:
+		bufferspace = sharedIndices[0][1]
+
+	for j in range(0, bufferspace):
+			bufferedTemplate.insert(0, None)
+
+	# now add end buffer if needed
+	end = (len(str1), len(str2))
+	secondToEnd = sharedIndices[len(sharedIndices) - 1]
+
+	if end[0] - secondToEnd[0] > end[1] - secondToEnd[1]:
+		bufferspace = end[0] - secondToEnd[0] - 1
+	else:
+		bufferspace = end[1] - secondToEnd[1] - 1
+
+	for j in range(0, bufferspace):
+			bufferedTemplate.append(None)
+
+
+	print "Buffered Template: "
+	print bufferedTemplate
+
+
+
+	return None
 
 def main():
-	w1 = "paine"
-	w2 = "cane"
+	w1 = "cranberry"
+	w2 = "randomly"
 
 	s = getListOfIndexMappings(w1, w2, 0, 0)
 	print "set: ", s
+
+	opt = {}
+	for d in s:
+		if len(d) > len(opt):
+			opt = d
+
+	print "\n\n"
+
+	both = getBufferedArrayFormats(w1, w2, opt)
+
 
 	# z = getOptimalZeroPosition(w2Com, w1Com)
 	# print "Optimal zero index of ", w1Com, " on ", w2Com, " --> ", z
